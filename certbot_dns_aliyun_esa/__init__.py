deb79c651feae7f81e51b181e1b54a9216cc55a5
Kangyang Ji <kyangconn@outlook.com>
2026-04-01 17:11:59 +0100

    fix import

--- certbot_dns_aliyun_esa/__init__.py
+++ certbot_dns_aliyun_esa/__init__.py
@@ -2,6 +2,10 @@
 Certbot DNS Aliyun ESA Plugin
 """
 
+from .dns_aliyun_esa import Authenticator
+
 __version__ = "0.1.0"
 __author__ = "Kangyang Ji"
 __email__ = "kyangconn@outlook.com"
+
+__all__ = ["Authenticator"]
